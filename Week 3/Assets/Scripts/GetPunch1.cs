fbf8e0af6e15298da2ad740a27c52ec8b9d382bc
youngjun0313 <zerojun0313@kaist.ac.kr>
2020-07-27 13:46:15 +0900

    good

---
+++ Week 3/Assets/Scripts/GetPunch1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPunch1 : MonoBehaviour
+{
+    BoxCollider boxCollider;
+
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GameObject.Find("Player2").GetComponent<BoxCollider>();
+        boxCollider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.G))
+        {
+			boxCollider.enabled = true;
+            Debug.Log("활성화 되야함");
+		}
+        else
+        {
+            boxCollider.enabled = false;
+        }
+    }
+
+}
---
+++ Week 3/Assets/Scripts/GetPunch2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPunch2 : MonoBehaviour
+{
+    BoxCollider boxCollider;
+
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GameObject.Find("Player1").GetComponent<BoxCollider>();
+        boxCollider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Period))
+        {
+			boxCollider.enabled = true;
+            Debug.Log("활성화 되야함");
+		}
+        else
+        {
+            boxCollider.enabled = false;
+        }
+    }
+
+}
